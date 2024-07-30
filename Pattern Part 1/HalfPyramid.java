e0530343ba396d90fc41f2cdffdcbd23e6142be4
Aman Patel <amanpatel.ajp@gmail.com>
2024-07-30 13:54:30 +0530

    Java Patterns Part 1

---
+++ Pattern Part 1/HalfPyramid.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class HalfPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number : ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}

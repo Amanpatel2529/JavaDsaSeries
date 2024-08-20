29517a3f3d63988620e0b47253648e76cadea19e
Aman Patel <amanpatel.ajp@gmail.com>
2024-08-20 13:15:42 +0530

    update Trapping rainwater file

--- Arrays/TrappingRainwater.java
+++ Arrays/TrappingRainwater.java
@@ -1,5 +1,5 @@
 /* Given n non-negative integers representing an elevation
-   map where the width if each bar is 1, 
+   map where the width of each bar is 1, 
    compute how much water it can trap after raining.
  */
 
